1158ec95257ac31ff72a60075e8d562864be9fc0
jinseok9338 <jinseok9338@gmail.com>
2021-05-22 10:26:09 +0000

    Second commit

--- pikasay/src/main.rs
+++ pikasay/src/main.rs
@@ -1,5 +1,6 @@
-use cursive;
-use cursive::views::TextView;
+use cursive::traits::Identifiable;
+use cursive::views::{Checkbox, Dialog, EditView, ListView, TextView};
+use cursive::event::{Key};
 
 const PIKACHU : &str = 
 "⣿⣿⣿⣿⣿⡏⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿
@@ -35,8 +36,71 @@ const POKEBALL : &str =
 ─────▀███░░░░░░░░░░░███▀─────
 ────────▀███████████▀────────";
 
+struct PikasayOptions<'a> {
+    message: &'a str,
+    pokeball: bool,
+}
+
+fn input_step(app: &mut cursive::Cursive) {
+    app.pop_layer();
+    app.add_global_callback(Key::Esc, |s| s.quit());
+    app.add_layer(
+        Dialog::new()
+            .title("Pika Pika !!!")
+            .content(
+                ListView::new()
+                    .child("Message:", EditView::new().with_name("message"))
+                    .child("Return!", Checkbox::new().with_name("return")),
+            )
+            .button("OK", |s| {
+                let message = s
+                    .call_on_name("message", |t: &mut EditView| t.get_content())
+                    .unwrap();
+                let returned = s
+                    .call_on_name("return", |t: &mut Checkbox| t.is_checked())
+                    .unwrap();
+                let options = PikasayOptions {
+                    message: &message,
+                    pokeball: returned,
+                };
+                result_step(s, &options)
+            })
+            .button("EXIT", |s| s.quit())
+    );
+}
+
+fn result_step(app: &mut cursive::Cursive, options: &PikasayOptions) {
+    let message = if options.message.is_empty() { "Created by danlogs!" } else { options.message };
+    let text = if options.pokeball {
+        format!(
+        "{}",
+        POKEBALL)
+    } else {
+        format!(
+" {}
+| {} |
+ {}
+  \\  /
+   \\/
+{}",
+        "-".repeat(message.chars().count() + 2),
+        message,
+        "-".repeat(message.chars().count() + 2),
+        PIKACHU)
+};
+
+    app.pop_layer();
+    app.add_layer(
+        Dialog::around(TextView::new(text))
+            .title("Pika...")
+            .button("OK", |s| input_step(s)),
+    );
+}
+
 fn main() {
     let mut app = cursive::default();
-    app.add_layer(TextView::new(PIKACHU));
+
+    input_step(&mut app);
+
     app.run();
 }
